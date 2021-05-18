1ad064e8a4f13e878d17aab78827970471f2634b
erveve <vladarushenko@gmail.com>
2021-05-18 12:59:31 +0300

    add few sorting algorithms, add methods emplementations

--- sequence.py
+++ sequence.py
@@ -1,4 +1,3 @@
-from ast import Num
 import pandas as pd
 from matplotlib import pyplot as plt
 from operations import *
@@ -11,7 +10,9 @@ sort_dict = {"nn": sort_nn,
              "nn_21_loops": sort_21_loops,
              "nn_21_no_loops": sort_21_no_loops,
              "polar": sort_angle,
-             "ch": sort_ch}
+             "ch": sort_ch,
+             "2-opt": two_opt,
+             "ui": sort_union_intersection}
 
 class Sequence():
     def __init__(self, data, contour=[]):
@@ -25,30 +26,52 @@ class Sequence():
 
     
     def have_loops(self, return_num=False):
-        return False
+        res = count_loops(self.data, self.contour, self.n)
+        if return_num:
+            return res>0, res
+        else:
+            return res>0
 
     
     def have_missed_data(self, return_num=False):
+        # missed_data = np.arange(self.n)
+        md = np.delete(np.arange(self.n), self.contour)
         if return_num:
-            return False, 0
-        return False
+            return len(md)>0, md
+        else:
+            return len(md)>0
 
     
     def is_data_unique(self, return_not_unique=False):
-        return True
-
-
-    def is_contour(self):
-        if not self.have_loops() and not self.have_missed_data:
-            return True
+        _, indexes = np.unique(self.data, return_index=True, axis=0)
+        res = np.delete(np.arange(self.n), indexes)
+        # set(range(self.n))-set(indexes)
+        if return_not_unique:
+            return len(res)==0, res
         else:
-            return False
+            return len(res)==0
+
+
+    def is_contour(self, return_problems=False):
+        loop = self.have_loops()
+        md = self.have_missed_data()
+        unique_data = self.is_data_unique()
+        res = True if not loop and not md and unique_data else False
+        if return_problems:
+            res_str = loop*'Have loops. '+md*'Have missed data. '+(not unique_data)*'Data is not unique.'
+            return res, res_str
+        else:
+            return res
+
+
+    def get_data_len(self):
+        return self.n
 
 
     def get_contour_len(self):
-        return len(self.contour)
+        return calc_path(self.data, self.sim, self.contour, self.n)
 
-    
+
     def sorted(self, key="nn_no_loops"):
         sort_ind = sort_dict[key](self.data, self.sim, self.n)
         sorted_data = Sequence(self.data, sort_ind)
@@ -57,13 +80,16 @@ class Sequence():
 
     def show_contour(self, save_fig=False, fig_name='figure'):
         have_md, md = self.have_missed_data(return_num=True)
+
+        plt.figure(num)
         if have_md:
             plt.scatter(self.data[md,0], self.data[md,1], c='g', s=30)
-        plt.figure(num)
         plt.scatter(self.data[self.contour,0],self.data[self.contour,1], c='b', s=30)
         plt.plot(self.data[self.contour,0],self.data[self.contour,1],'r')
+        if fig_name=='polar':
+            plt.scatter(np.sum(self.data[:, 0])/self.n,np.sum(self.data[:, 1])/self.n, c='y', s=30)
         if save_fig:
-            plt.savefig(fig_name+".png")
+            plt.savefig("images/"+fig_name+".png")
         else:
             plt.show()        
 
